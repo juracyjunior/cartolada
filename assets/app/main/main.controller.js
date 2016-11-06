eec6ce151f021084c3d5c5ae3450c88d2be33aaf
Juracy Junior <jfcjuniorr@gmail.com>
2016-11-06 21:37:28 -0200

    Ajustes

--- assets/app/main/main.controller.js
+++ assets/app/main/main.controller.js
@@ -30,34 +30,15 @@
 
         function loginGlobo()
         {
-            Cadun.login(438, location.href, false, function(data) {
-                console.log(data);
-                //window.location.href = self.homeLogadaUrl;
-            });
-            
-            
-            /*Cadun.authorize(438, function(user) {
+            Cadun.authorize(438, function(user) {
                 if (user.status == 'authorized') {
-                    //window.location.href = self.homeLogadaUrl;
-                    console.log(user);
+                    window.location.href = self.homeLogadaUrl;
                 } else {
                     Cadun.login(438, location.href, false, function(data) {
-                        console.log("logoff");
-                        //window.location.href = self.homeLogadaUrl;
+                        window.location.href = self.homeLogadaUrl;
                     });
                 }
-            });*/
-
-            /*Cadun.authorize(Cadun.serviceId, function(h) {
-                Cadun.user.me = h;
-                if (h !== null && h.token !== null) {
-                    Cadun.util.setCookie("GLBID", h.token)
-                }
-                if (typeof(Cadun.callback) == "function") {
-                    Cadun.callback(h);
-                    Cadun.callback = null
-                }
-            });*/
+            });
         }
     }
 })();
